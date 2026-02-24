504adce68221bd8b755daf1b06bfef2444a3eeb1
layane0814 <layane.coelho@etec.sp.gov.br>
2026-02-24 11:17:40 -0300

    24/2

---
+++ exemplo1.js
@@ -0,0 +1,7 @@
+document.write("<h2>Entrada de Dados</h2>");
+
+var numero1 = prompt("Digite o número 1");
+var numero2 = prompt("Digite o número 2");
+
+document.write("Número 1 = " + numero1 + "<br>");
+document.write("Número 2 = " + numero2 + "<br>");
